5a7ceb5ec7eea9948dd96bfd9334eb5679cb6887
CharlieWilliams27099 <charliewilliams27099@gmail.com>
2025-07-01 11:55:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function startGame() {
   alert("Career mode launching soon...");
   // Later, you’ll navigate to the game screen here
 }
+button.addEventListener("click", () => {
+  localStorage.setItem("selectedTeam", team);
+  localStorage.setItem("selectedLeague", selectedLeague);
+  window.location.href = "dashboard.html";
+});
